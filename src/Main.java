7b9585a4a16e60aacd71a8f0229cef7a79256998
José Rodriguez <jard3r5@gmail.com>
2024-05-18 08:52:28 -0600

    Ejercicio 2 usando switch

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,31 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner teclado=new Scanner(System.in);
-        /*int edad;
+        int opcion;
+        System.out.println("un numero entre 1 y 7 ");
+        System.out.print("1: Eres mayor de edad?"+"\n"+
+                "2: numero mayor" +"\n" +
+                "3: par o impar" + "\n"+
+                "4:factorial " + "\n"+
+                "5: numeros impares usando for" + "\n"+
+                "6: numero impares usando while" + "\n"+
+                "7: dia de la semana");
+        opcion=teclado.nextInt();
+        switch(opcion) {
+            case 1:
 
-            System.out.println("Ingrese su edad");
-            edad=teclado.nextInt();
-            if(edad>18){
-                System.out.println("Eres mayor de edad");
-            }else{System.out.println("Eres menor de edad");}*/
-        /*int num1 ,nume2;
+                int edad;
+
+                System.out.println("Ingrese su edad");
+                edad = teclado.nextInt();
+                if (edad > 18) {
+                    System.out.println("Eres mayor de edad");
+                } else {
+                    System.out.println("Eres menor de edad");
+                }
+                break;
+                case 2:
+        int num1 ,nume2;
         System.out.println("Ingrese el primer numero");
         num1=teclado.nextInt();
         System.out.println("Ingrese el segundo numero");
@@ -20,8 +37,9 @@ public class Main {
         }
         else {
             System.out.println(nume2 + " Es el numero mayor");
-        }*/
-        /*
+        }break;
+        case 3:
+
         int num;
         System.out.println("Ingrese un numero: ");
         num=teclado.nextInt();
@@ -29,18 +47,18 @@ public class Main {
             System.out.println("El numero es par");
         }else {
             System.out.println("El numero es impar");
-        }*/
-        /*
-        int factorial =0, num, multiplicacion;
+        }break;
+        case 4:
+        int factorial =0, nume;
         System.out.println("Ingrese un numero: ");
-        num=teclado.nextInt();
-        factorial=num;
-        for (int i = 1; i < num; ++i) {
+        nume=teclado.nextInt();
+        factorial=nume;
+        for (int i = 1; i < nume; ++i) {
             factorial=factorial*i;
 
             System.out.println(factorial);
-        }*/
-        /*
+        }break;
+        case 5:
         int numero;
         System.out.println("Ingrese un numero mayor que 10 y menor que 30");
         numero=teclado.nextInt();
@@ -48,8 +66,8 @@ public class Main {
         numero=teclado.nextInt();}
         for(int i=1;i<=numero;i+=2){
             System.out.println(i);
-        }*/
-/*
+        }break;
+case 6:
         int k =1, numer;
         System.out.println("Ingrese un numero mayor que 10 y menor que 30");
         numer=teclado.nextInt();
@@ -58,28 +76,32 @@ public class Main {
         while(k<numer){
             k+=2;
                     System.out.println(k);
-        }*/
-        int nm;
-        System.out.println("Ingrese un numero entre 1 y 5");
-        nm=teclado.nextInt();
-        while(nm<1 || nm>5){System.out.println("Ingrese un numero entre 1 y 5");
-        nm=teclado.nextInt();}
-        switch(nm){
-        case 1:
-        System.out.println("Lunes");
-        break;
-        case 2:
-            System.out.println("Martes");
-            break;
-            case 3:
-                System.out.println("Miercoles");
-                break;
-                case 4:
-                    System.out.println("Jueves");
-                    break;
+        }break;
+        case 7:
+                int nm;
+                System.out.println("Ingrese un numero entre 1 y 5");
+                nm = teclado.nextInt();
+                while (nm < 1 || nm > 5) {
+                    System.out.println("Ingrese un numero entre 1 y 5");
+                    nm = teclado.nextInt();
+                }
+                switch (nm) {
+                    case 1:
+                        System.out.println("Lunes");
+                        break;
+                    case 2:
+                        System.out.println("Martes");
+                        break;
+                    case 3:
+                        System.out.println("Miercoles");
+                        break;
+                    case 4:
+                        System.out.println("Jueves");
+                        break;
                     case 5:
                         System.out.println("Viernes");
                         break;
+                }break;
         }
     }
     }
