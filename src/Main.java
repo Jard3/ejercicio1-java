9c2d2e484fe6e9032c90dc07aa263c599855061a
José Rodriguez <jard3r5@gmail.com>
2024-05-18 07:54:08 -0600

    Ejercicio 2 usando if

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,25 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        int edad;
         Scanner teclado=new Scanner(System.in);
+        /*int edad;
+
             System.out.println("Ingrese su edad");
             edad=teclado.nextInt();
             if(edad>18){
                 System.out.println("Eres mayor de edad");
-            }else{System.out.println("Eres menor de edad");}
+            }else{System.out.println("Eres menor de edad");}*/
+        int num1 ,nume2;
+        System.out.println("Ingrese el primer numero");
+        num1=teclado.nextInt();
+        System.out.println("Ingrese el segundo numero");
+        nume2=teclado.nextInt();
+        if (num1==nume2){System.out.println("Los numeros son iguales");}
+        else if (num1>nume2){
+            System.out.println(num1 + " Es el numero mayor");
         }
+        else {
+            System.out.println(nume2 + " Es el numero mayor");
+        }
+    }
     }
